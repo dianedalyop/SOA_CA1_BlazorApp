5434e2358e0c44a00b959156b9ad52990ed4cb67
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-10-23 21:02:40 +0100

    Meal class setup

---
+++ Models/Meal.cs
@@ -0,0 +1,16 @@
+﻿namespace SOA_CA1_BlazorApp.Models
+{
+    public class Meal
+    {
+        public string sMeal { get; set; }
+        public string sCategory { get; set; }
+        public string sArea { get; set; }
+        public string sInstructions { get; set; }
+        public string sMealThumb { get; set; }
+    }
+
+    public class MealResponse
+    {
+        public List<Meal> meals { get; set; }
+    }
+}
