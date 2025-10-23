ddfb3409592a9b15149a92b2ef1ad54633d1af07
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-10-23 21:43:01 +0100

    Registering meal Service

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace SOA_CA1_BlazorApp
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<SOA_CA1_BlazorApp.Services.MealService>();
+
+           // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
