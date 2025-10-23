e01f955240156994942a2a5d444733493352390b
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-10-23 21:09:46 +0100

    Meal Services and API setup

---
+++ Services/MealService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+using SOA_CA1_BlazorApp.Models;
+
+namespace SOA_CA1_BlazorApp.Services
+{
+    //  interact with TheMealDB API
+    public class MealService
+    {
+        private readonly HttpClient _http;
+
+        public MealService(HttpClient http)
+        {
+            _http = http;
+        }
+
+
+        // Searching for meals by name
+        public async Task<List<Meal>> SearchMealsAsync(string query)
+        {
+
+            try // in case of network issues or invalid responses
+            {
+                var response = await _http.GetFromJsonAsync<MealResponse>(
+                    $"https://www.themealdb.com/api/json/v1/1/search.php?s={query}"
+                );
+                return response?.meals ?? new List<Meal>();
+            }
+            catch
+            {
+                return new List<Meal>();
+            }
+        }
+    }
+}
