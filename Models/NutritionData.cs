748eb7b4ff29dd851f43adb0722b2528bb8e14f4
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-10-24 03:01:29 +0100

    Resolving issues with NutritionDataServices

---
+++ Models/NutritionData.cs
@@ -0,0 +1,24 @@
+﻿namespace SOA_CA1_BlazorApp.Models
+{
+    public class NutritionData
+    {
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double CholesterolMg { get; set; }
+        public double CarbohydratesTotalG { get; set; }
+        public double FiberG { get; set; }
+        public double SugarG { get; set; }
+
+        public double FatTotalG { get; set; }
+        public double FatSaturatedG { get; set; }
+        public double ProteinG { get; set; }
+        public double SodiumMg { get; set; }
+        public double PotassiumMg { get; set; }
+        
+    }
+
+    public class NutritionApiResponse
+    {
+        public List<NutritionData> Items { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,8 +15,10 @@ namespace SOA_CA1_BlazorApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<SOA_CA1_BlazorApp.Services.MealService>();
+            builder.Services.AddScoped<NutritionDataService>();
 
-           // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
